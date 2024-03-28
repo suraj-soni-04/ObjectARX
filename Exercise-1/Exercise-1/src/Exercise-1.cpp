f52e65f1cf82b9fbc26bb4e2649e53af1d3dd3fc
Suraj Soni <suraj.soni@cctech.co.in>
2024-03-28 12:53:34 +0530

    exercise-1

---
+++ Exercise-1/Exercise-1/src/Exercise-1.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "framework.h"
+#include "arxHeaders.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+void MyCommand()
+{
+	acutPrintf(_T("\n Hello User!!!"));
+}
+
+
+extern "C" AcRx::AppRetCode
+acrxEntryPoint(AcRx::AppMsgCode Msg, void* pkt)
+{
+	switch (Msg)
+	{
+	case AcRx::kInitAppMsg:
+		acrxUnlockApplication(pkt);
+		acrxRegisterAppMDIAware(pkt);
+		acutPrintf(L"\n Command Loaded");
+		acedRegCmds->addCommand(L"MyGroup", _T("MYCOMMAND"), _T("MYCOMMAND"), ACRX_CMD_MODAL, MyCommand);
+		break;
+
+	case AcRx::kUnloadAppMsg:
+		acutPrintf(_T("\nUnloading createLine project...\n"));
+		// Command Groups to remove
+		acedRegCmds->removeGroup(_T("AUCommnds"));
+		break;
+	default:
+		break;
+	}
+	return AcRx::kRetOK;
+}
